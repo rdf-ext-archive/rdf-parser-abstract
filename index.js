ad80e6802b4a4231e759fcdd43901a442282cabe
bergi <bergi@axolotlfarm.org>
2015-09-05 21:55:49 +0200

    use standard code style

--- index.js
+++ index.js
@@ -1,99 +1,94 @@
-var
-  util = require('util'),
-  Readable = require('stream').Readable;
+var util = require('util')
+var Readable = require('stream').Readable
 
-
-var AbstractParser = function (rdf) {
-  var self = this;
+function AbstractParser (rdf) {
+  var self = this
 
   this.rdf = rdf
 
   this.parse = function (data, callback, base, filter, graph) {
-    graph = graph || self.rdf.createGraph();
+    graph = graph || self.rdf.createGraph()
 
     var pushTriple = function (triple) {
-      graph.add(triple);
-    };
+      graph.add(triple)
+    }
 
     return new Promise(function (resolve, reject) {
       self.process(data, pushTriple, base, filter, function (error) {
         // callback API
         if (callback) {
-          callback(error, graph);
+          callback(error, graph)
         }
 
         // Promise API
         if (error) {
-          reject(error);
+          reject(error)
         } else {
-          resolve(graph);
+          resolve(graph)
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
   this.stream = function (inputStream, base, filter) {
-    var outputStream = new AbstractParser.TripleReadStream();
+    var outputStream = new AbstractParser.TripleReadStream()
 
     AbstractParser.streamToData(inputStream).then(function (data) {
       self.process(data, function (triple) {
-        outputStream.push(triple);
+        outputStream.push(triple)
       }, base, filter, function (error) {
         if (error) {
-          outputStream.emit('error', error);
+          outputStream.emit('error', error)
         } else {
-          outputStream.emit('end');
+          outputStream.emit('end')
         }
-      });
+      })
     }).catch(function (error) {
-      outputStream.emit('error', error);
-    });
+      outputStream.emit('error', error)
+    })
 
-    return outputStream;
-  };
-};
-
+    return outputStream
+  }
+}
 
 AbstractParser.streamToData = function (stream) {
   return new Promise(function (resolve, reject) {
     if (typeof stream !== 'object' || typeof stream.read !== 'function') {
-      return resolve(stream);
+      return resolve(stream)
     }
 
-    var data;
+    var data
 
     stream.on('data', function (chunk) {
       if (!data) {
-        data = chunk;
+        data = chunk
       } else {
         if (Buffer.isBuffer(data)) {
-          data = Buffer.concat(data, chunk);
+          data = Buffer.concat(data, chunk)
         } else {
-          data += data.toString();
+          data += data.toString()
         }
       }
     })
 
-    stream.on('end', function() {
-      resolve(data);
-    });
+    stream.on('end', function () {
+      resolve(data)
+    })
 
     stream.on('error', function (error) {
-      reject(error);
-    });
-  });
-};
-
+      reject(error)
+    })
+  })
+}
 
 AbstractParser.TripleReadStream = function () {
-  Readable.call(this, {objectMode: true});
+  Readable.call(this, {objectMode: true})
 
   this._read = function () {
-    return 0;
-  };
-};
+    return 0
+  }
+}
 
-util.inherits(AbstractParser.TripleReadStream, Readable);
+util.inherits(AbstractParser.TripleReadStream, Readable)
 
-
-module.exports = AbstractParser;
+module.exports = AbstractParser
